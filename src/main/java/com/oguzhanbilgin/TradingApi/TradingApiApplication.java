4df7f246947b9fadc58a42e34a6746c4b554bc27
ogzhnblgnn <ogzhnbxx@gmail.com>
2023-12-21 12:28:32 +0300

    init commit

---
+++ src/main/java/com/oguzhanbilgin/TradingApi/TradingApiApplication.java
@@ -0,0 +1,13 @@
+package com.oguzhanbilgin.TradingApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oguzhanbilgin/TradingApi/TradingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oguzhanbilgin.TradingApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
